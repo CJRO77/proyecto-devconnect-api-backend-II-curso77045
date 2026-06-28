8a72ee2998187d731233ca3e02416cc9b490c0b6
Carlos Rodriguez <jonarodriguez32@gmail.com>
2026-06-28 15:16:04 -0300

    endpoint para crear, guardar, modificar y eliminar usuarios

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -50,3 +50,95 @@ export const createUser = async (req, res) => {
     });
   }
 };
+
+// Obtener usuario por ID
+
+export const getUserById = async (req, res) => {
+  try {
+
+    const user = await UserModel.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Usuario no encontrado",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error al obtener usuario",
+      error: error.message,
+    });
+  }
+};
+
+// buscar usuario por ID y actualizarlo
+
+export const updateUser = async (req, res) => {
+  try {
+
+    const user = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Usuario no encontrado",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Usuario actualizado correctamente",
+      data: user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error al actualizar usuario",
+      error: error.message,
+    });
+  }
+};
+
+// buscar usuario por ID y eliminarlo
+
+export const deleteUser = async (req, res) => {
+  try {
+
+    const user = await UserModel.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Usuario no encontrado",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Usuario eliminado correctamente",
+      data: user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error al eliminar usuario",
+      error: error.message,
+    });
+  }
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
-import { getUsers,createUser } from "../controllers/users.controller.js";
+import { getUsers,createUser,getUserById,updateUser,deleteUser} from "../controllers/users.controller.js";
 
 const router = Router();
 
 // Obtener todos los usuarios
 router.get("/", getUsers);
+router.get("/:id", getUserById);
 
 // Crear un nuevo usuario
 router.post("/", createUser);
+
+// Actualizar un usuario existente
+router.put("/:id", updateUser);
+
+// Eliminar un usuario existente
+router.delete("/:id", deleteUser);
 
 
 export default router;
